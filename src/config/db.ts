66261e854a95cfd641a1b07862ebb8f682653c80
noboKumar <nobokumar227@gmail.com>
2025-12-07 02:33:07 +0600

    add sql logic to pool

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,7 @@ import { Pool } from "pg";
 import config from ".";
 
 export const pool = new Pool({
-  connectionString: config.connectionStr,
+  connectionString: `${config.connectionStr}`,
 });
 
 const initDB = async () => {
@@ -14,11 +14,41 @@ const initDB = async () => {
         email VARCHAR(255) UNIQUE NOT NULL,
         CHECK  (email = lower(email)),
         password  TEXT NOT NULL,
+        CHECK(length(password) >= 6),
         phone VARCHAR(11) NOT NULL,
-        role VARCHAR(50) NOT NULL DEFAULT "customer",
-        CHECK (role IN ("admin", "customer")),
+        role VARCHAR(50) NOT NULL DEFAULT 'customer',
+        CHECK (role IN ('admin', 'customer'))
         )
         `);
+
+  // vehicles
+  await pool.query(`
+        CREATE TABLE IF NOT EXISTS vehicles(
+        id SERIAL PRIMARY KEY,
+        vehicle_name VARCHAR(100) NOT NULL,
+        type VARCHAR(50) NOT NULL,
+        CHECK (type IN ('car', 'bike', 'van', 'SUV')),
+        registration_number VARCHAR(50) NOT NULL UNIQUE,
+        daily_rent_price NUMERIC(10,2) NOT NULL CHECK(daily_rent_price > 0),
+        availability_status VARCHAR(20) NOT NULL,
+        CHECK (availability_status in ('available', 'booked'))
+        )
+        `);
+
+  // bookings
+  await pool.query(`
+            CREATE TABLE IF NOT EXISTS bookings(
+            id SERIAL PRIMARY KEY,
+            customer_id INT REFERENCES users(id) ON DELETE CASCADE,
+            vehicle_id INT REFERENCES vehicles(id) ON DELETE CASCADE,
+            rent_start_date DATE NOT NULL,
+            rent_end_date DATE NOT NULL,
+            CHECK(rent_end_date > rent_start_date),
+            total_price NUMERIC(10,2) NOT NULL,
+            status VARCHAR(20) NOT NULL,
+            CHECK (status IN ('active', 'cancelled', 'returned'))
+            )
+            `);
 };
 
 export default initDB;
